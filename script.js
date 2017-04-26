c74a623cb0914471f948bb0bb60ee5070a62447d
younesshakky <youness.hakky@gmail.com>
2017-04-26 13:13:11 +0100

    adding show preview feature (static)

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ var canvas = document.querySelector('canvas'),
     canvasWrap = document.querySelector('.main_canvas'),
     canvasHold = document.querySelector('.canvas_placeholder'),
     imgDefault = new Image();
-    imgDefault.src = 'https://www.mememaker.net/static/images/templates/198942.jpg';
+
+imgDefault.src = 'https://www.mememaker.net/static/images/templates/198942.jpg';
 
 var x = 0,
     y = 0;
+
+var canvasImageWidth = document.getElementById('img_size-width').value,
+    canvasImageHeight = document.getElementById('img_size-height').value;
 
 // inputs related
 var imgURLField = document.getElementById('img_URL'),
@@ -21,39 +25,22 @@ canvasWrap.style.height = canvas.height + 'px';
 canvasHold.style.width = canvas.width + 'px';
 canvasHold.style.height = canvas.height + 'px';
 
-window.onload = function(){
-    return setDefaultImg(imgDefault, x, y);
-}
+window.onload = setDefaultImg(imgDefault, x, y);
+
 
 function setDefaultImg (img, x, y, swidth, sheight) {
     x = this.x
     y = this.y
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, x, y, canvas.width, canvas.width);
-    console.log(sheight, sheight);
+    ctx.drawImage(img, x, y, canvasImageWidth, canvasImageHeight);
+    console.log(canvasImageWidth, canvasImageHeight);
+
     // console.log('x axis : ' + x, 'y axis : ' + y);
-    // for debugging
 }
 
+// validating img
 
-// moving arround
-
-function moveUp () {
-    return setDefaultImg(imgDefault, x, y-=2);
-}
-function moveDown () {
-    return setDefaultImg(imgDefault, x, y+=2);
-}
-
-function moveLeft () {
-    return setDefaultImg(imgDefault, x-=2, y);
-}
-
-function moveRight () {
-    return setDefaultImg(imgDefault, x+=2, y);
-}
-
-imgURLField.oninput = function () {
+pushImg.onclick = function () {
     var sImgURL = imgURLField.value.split('.'),
         imgURLExt = sImgURL[sImgURL.length - 1];
 
@@ -61,10 +48,36 @@ imgURLField.oninput = function () {
         console.log('please insert a valid image');
     }else if (imgURLExt === 'png' || imgURLExt === 'jpg' || imgURLExt === 'jpeg'){
         imgDefault.src = imgURLField.value;
-        setDefaultImg(imgDefault, x, y);
+        setDefaultImg(imgDefault, x=0, y=0);
+    }else{
+        console.log('please insert a valid image');        
     }
 }
 
-// test cases:
-// http://nodeframework.com/assets/img/js.png
-// https://www.mememaker.net/static/images/templates/198942.jpg
+// moving arround
+
+function moveUp () {
+    setDefaultImg(imgDefault, x, y-=4);
+}
+function moveDown () {
+    setDefaultImg(imgDefault, x, y+=4);
+}
+
+function moveLeft () {
+    setDefaultImg(imgDefault, x-=4, y);
+}
+
+function moveRight () {
+    setDefaultImg(imgDefault, x+=4, y);
+}
+
+function togglePreview(){
+    var el = document.querySelector('.preview_wrapper');
+    el.classList.toggle('is_hidden');
+}
+
+/*
+images
+http://nodeframework.com/assets/img/js.png
+https://www.mememaker.net/static/images/templates/198942.jpg
+*/
